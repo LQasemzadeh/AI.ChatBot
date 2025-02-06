e83498a85910ae3dae5a6f5d84f8127317cb7c5a
lqasemzadeh <lqasemzadeh@gmail.com>
2025-02-06 21:34:46 +0100

    Refactor project and integrate TailwindCSS with improvements
    Removed unused assets and styling while updating the project structure to use TailwindCSS for styling. Introduced new React-Icons dependency and added a chatbot component with a corresponding icon. All updates prioritize a cleaner, modular project setup and modern UI enhancements.

---
+++ src/Components/ChatbotIcon.tsx
@@ -0,0 +1,11 @@
+import { TbMessageChatbotFilled } from "react-icons/tb";
+
+const ChatbotIcon = () => {
+    return (
+        <div>
+            <TbMessageChatbotFilled />
+        </div>
+    );
+};
+
+export default ChatbotIcon;
